aa466fb67196d4a107e857d06713c42a0c05ef4d
patchamas <patchamas.k@gmail.com>
2015-12-15 01:26:49 +0700

    Bug

---
+++ BUG/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace BUG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNoBug = new System.Windows.Forms.TextBox();
+            this.txtNoCondition = new System.Windows.Forms.TextBox();
+            this.txtCondition = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddCondition = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.gridCondition = new System.Windows.Forms.DataGridView();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gridResult = new System.Windows.Forms.DataGridView();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCondition)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNoBug
+            // 
+            this.txtNoBug.Location = new System.Drawing.Point(205, 16);
+            this.txtNoBug.Name = "txtNoBug";
+            this.txtNoBug.Size = new System.Drawing.Size(100, 20);
+            this.txtNoBug.TabIndex = 0;
+            // 
+            // txtNoCondition
+            // 
+            this.txtNoCondition.Location = new System.Drawing.Point(205, 39);
+            this.txtNoCondition.Name = "txtNoCondition";
+            this.txtNoCondition.Size = new System.Drawing.Size(100, 20);
+            this.txtNoCondition.TabIndex = 1;
+            // 
+            // txtCondition
+            // 
+            this.txtCondition.Location = new System.Drawing.Point(205, 77);
+            this.txtCondition.Name = "txtCondition";
+            this.txtCondition.Size = new System.Drawing.Size(100, 20);
+            this.txtCondition.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "#Bug --> จำนวนติดบัค (โหนด):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "#Path --> จำนวนเงื่อนไข (เส้น):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Condition (T CCC RRR) :";
+            // 
+            // btnAddCondition
+            // 
+            this.btnAddCondition.Location = new System.Drawing.Point(205, 103);
+            this.btnAddCondition.Name = "btnAddCondition";
+            this.btnAddCondition.Size = new System.Drawing.Size(100, 23);
+            this.btnAddCondition.TabIndex = 7;
+            this.btnAddCondition.Text = "Add";
+            this.btnAddCondition.UseVisualStyleBackColor = true;
+            this.btnAddCondition.Click += new System.EventHandler(this.btnAddCondition_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(6, 117);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 13);
+            this.lbResult.TabIndex = 8;
+            // 
+            // gridCondition
+            // 
+            this.gridCondition.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridCondition.Location = new System.Drawing.Point(12, 141);
+            this.gridCondition.Name = "gridCondition";
+            this.gridCondition.Size = new System.Drawing.Size(293, 141);
+            this.gridCondition.TabIndex = 9;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(205, 288);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(100, 23);
+            this.btnProcess.TabIndex = 10;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "PS --> T: Time, C: Condition, R: Result";
+            // 
+            // gridResult
+            // 
+            this.gridResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridResult.Location = new System.Drawing.Point(311, 16);
+            this.gridResult.Name = "gridResult";
+            this.gridResult.Size = new System.Drawing.Size(573, 266);
+            this.gridResult.TabIndex = 12;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(784, 288);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(745, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 327);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.gridResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.gridCondition);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnAddCondition);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCondition);
+            this.Controls.Add(this.txtNoCondition);
+            this.Controls.Add(this.txtNoBug);
+            this.Name = "Form1";
+            this.Text = "Bug Feature";
+            ((System.ComponentModel.ISupportInitialize)(this.gridCondition)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNoBug;
+        private System.Windows.Forms.TextBox txtNoCondition;
+        private System.Windows.Forms.TextBox txtCondition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAddCondition;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.DataGridView gridCondition;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView gridResult;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ BUG/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// ARRAY LIST
+using System.Collections;
+
+namespace BUG
+{
+    public partial class Form1 : Form
+    {
+        string[] arrBugState = new string[3];
+        DataTable dtPCondition = new DataTable();
+        DataTable dtResult = new DataTable();
+        int intTime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Input: Array [Extended Function]
+        private string ConvertCharToString(char[] array)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char value in array)
+            {   builder.Append(value);  }
+            return builder.ToString();
+        }
+
+        // Input: Number of Bug and Length of All eg. 3 of 4
+        private string setBugState(int intNoBug, int intLegth)
+        {
+            char[] arrBugPattern = new char[intLegth];
+            for (int i = 0; i < intLegth; i++)
+            {
+                if (i >= intNoBug)
+                { arrBugPattern[i] = '-'; }
+                else
+                { arrBugPattern[i] = '+'; }
+            }
+            return ConvertCharToString(arrBugPattern);
+        }
+
+        // Input: Codition and BugValue
+        private bool checkBugCondition(string strCondition, string strBug) 
+        {
+            bool boolMatch = true;
+            char[] arrConditionChar = strCondition.ToCharArray();
+            char[] arrBug = strBug.ToCharArray();
+
+            if(arrBug.Length == arrConditionChar.Length)
+            {
+                for (int i = 0; i < arrConditionChar.Length; i++)
+                {
+                    if (arrConditionChar[i] != '0') // Not Ignore
+                    {
+                        if (arrConditionChar[i] == '-')
+                        {
+                            if (arrBug[i] == '+')
+                            { boolMatch = false; }
+                        }
+                        else if (arrConditionChar[i] == '+')
+                        {
+                            if (arrBug[i] == '-')
+                            { boolMatch = false; }
+                        }
+                    }
+                }
+            }
+            
+            return boolMatch;
+        }
+
+        // Input : ConditionResult and BugValue
+        private string calculateResult(string strConditionResult, string strBug) 
+        {
+            char[] arrConditionResult = strConditionResult.ToCharArray();
+            char[] arrBug = strBug.ToCharArray();
+            char[] arrResult = new char[arrBug.Length];
+
+            for (int i = 0; i < arrBug.Length; i++) 
+            {
+                if ((arrBug[i] == '+') && (arrConditionResult[i] == '-')) // Reduce bug
+                { arrResult[i] = '-'; }
+                else if ((arrConditionResult[i] == '+')) // Add bug
+                { arrResult[i] = '+'; }
+                else
+                { arrResult[i] = arrBug[i]; }                
+            }
+            return ConvertCharToString(arrResult);
+        }
+
+        // Input: All-Condition and Mode
+        private string getValueOfCondition(string strCondition, string strOption)
+        {
+            string[] arrCondition = strCondition.Split(' ');
+            if (strOption == "T")
+            { return arrCondition[0]; }
+            else if (strOption == "C")
+            { return arrCondition[1]; }
+            else if (strOption == "R")
+            { return arrCondition[2]; }
+            else
+            { return ""; }
+        }
+
+        // Input: Array Bug
+        private bool checkBugNone(char[] arrBug)
+        {
+            bool bookBugNone = true;
+            for (int i = 0; i < arrBug.Length; i++)
+            {
+                if (bookBugNone == false)
+                { break; }
+
+                if (arrBug[i] == '+')
+                { bookBugNone = false; }
+            }
+            return bookBugNone;
+        }
+
+        private bool checkSameLastPath(string strNowCondition)
+        {
+            bool boolSame = false;
+
+            if (dtResult.Rows.Count > 0)
+            {
+                if ((dtResult.Rows[dtResult.Rows.Count - 1]["Codition"].ToString() != strNowCondition))
+                { boolSame= true; }
+                else
+                {
+                    if (dtResult.Rows.Count >= 2)
+                    {
+                        if ((dtResult.Rows[dtResult.Rows.Count - 2]["Codition"].ToString() != strNowCondition))
+                        { boolSame= true; }
+                    }
+                }                
+            }
+            else
+            { boolSame= true; }
+
+            return boolSame;
+        }
+
+        private bool checkExit()
+        {
+            bool boolSame = true;
+
+            if (dtResult.Rows.Count > 0)
+            {
+                if (checkBugNone(dtResult.Rows[dtResult.Rows.Count - 1]["Result"].ToString().ToCharArray()) == true)
+                { boolSame = false; }
+            }
+
+            return boolSame;
+        }
+
+        // Event 'ADD' Click
+        private void btnAddCondition_Click(object sender, EventArgs e)
+        {
+            if (dtPCondition.Columns.Count == 0)
+            { dtPCondition.Columns.Add("Condition", typeof(string)); }
+
+            if (dtPCondition.Rows.Count < Convert.ToInt16(txtNoCondition.Text)) // Input not more than config number of Path
+            {
+                DataRow dr = dtPCondition.NewRow();
+                dr["Condition"] = txtCondition.Text;
+                dtPCondition.Rows.Add(dr);
+
+                gridCondition.DataSource = dtPCondition.DefaultView;
+                gridCondition.Refresh();
+
+                txtCondition.Text = "";
+            }
+        }
+
+        // Event 'Process' Click
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (dtResult.Columns.Count == 0)
+            { 
+                dtResult.Columns.Add("Input", typeof(string));
+                dtResult.Columns.Add("Codition", typeof(string));
+                dtResult.Columns.Add("Result", typeof(string));
+                dtResult.Columns.Add("Time", typeof(string));
+            }
+
+            string strBug = setBugState(Convert.ToInt16(txtNoBug.Text), Convert.ToInt16(txtNoCondition.Text));
+            Process(strBug);
+        }
+
+        private void Done()
+        {
+            gridResult.DataSource = dtResult.DefaultView;
+            gridResult.Refresh();
+
+            int intTime = 0;
+            for(int i = 0; i<dtResult.Rows.Count;i++)
+            {intTime += Convert.ToInt16(dtResult.Rows[i]["Time"].ToString());}
+
+            txtTime.Text = intTime.ToString();
+        }
+
+        private void Process(string strBug)
+        {
+            if (checkBugNone(strBug.ToCharArray()) == false)
+            {
+                for (int i = 0; i < dtPCondition.Rows.Count; i++)
+                {
+                    if (checkExit())
+                    {
+                        string strCondition = getValueOfCondition(dtPCondition.Rows[i]["Condition"].ToString(), "C");
+                        if (checkSameLastPath(strCondition)) // not allow use 2 time as near by 
+                        {
+                            if (checkBugCondition(strCondition, strBug) == true) // Can use this path
+                            {
+                                string strConditionResult = getValueOfCondition(dtPCondition.Rows[i]["Condition"].ToString(), "R");
+                                string strResult = calculateResult(strConditionResult, strBug);
+                                string strTime = getValueOfCondition(dtPCondition.Rows[i]["Condition"].ToString(), "T");
+                                intTime += Convert.ToInt16(strTime);
+
+                                DataRow dr = dtResult.NewRow();
+                                dr["Input"] = strBug;
+                                dr["Codition"] = strCondition;
+                                dr["Result"] = strResult;
+                                dr["Time"] = strTime;
+                                dtResult.Rows.Add(dr);
+
+                                if (checkBugNone(strResult.ToCharArray()) == false)
+                                { Process(strResult); }
+                                else
+                                {
+                                    Done();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    { break; }
+                }
+            }
+            
+        }
+    }
+}
